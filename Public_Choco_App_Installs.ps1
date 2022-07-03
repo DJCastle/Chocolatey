7405714c7a8f3e02445209a98a58024f00dbb46b
Don Castle <doncastle@me.com>
2022-07-02 17:47:37 -0700

    First Committ

---
+++ Public_Choco_App_Installs.ps1
@@ -0,0 +1,20 @@
+## Install script for Boyd School of Law
+
+# Configure chocolatey - Set Global Confirmation to YES/ALL
+choco feature enable -n allowGlobalConfirmation
+
+#choco Set Update Schedule
+choco install choco-upgrade-all-at --params "'/WEEKLY:yes /DAY:SUN /TIME:01:00'"
+
+## Softare Install
+choco install 7zip
+#choco install vstor2010
+#choco install GoogleChrome
+#choco install Firefox
+#choco install vlc
+#choco install zoom
+#choco install webex-meetings
+#choco install microsoft-edge
+ 
+# Configure chocolatey - Turn off Global Confirmaiton
+choco feature disable -n allowGlobalConfirmation
---
+++ README.md
@@ -0,0 +1,2 @@
+# Chocolatey Scripts to install setup and schedule updates
+ Chocolatey Scripts used at the William S. Boyd School of Law to install, schedule updates and update non-Microsoft apps.
